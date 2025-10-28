0047f9a3bf007488e76f645410fb0697f3a1ee82
glebrazbegaev <darclose@vk.ru>
2025-10-28 18:05:36 +0300

    Сортировка пузырьком.py

--- Сортировка пузырьком.py
+++ Сортировка пузырьком.py
@@ -1,10 +1,55 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n-1):
-        for j in range(n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+def bubble_sort(sequence):
+    """
+    Реализация сортировки пузырьковым методом
+    """
+    # Определяем количество элементов
+    count = len(sequence)
+    
+    # Основные проходы по массиву
+    for pass_num in range(count - 1):
+        
+        # Сравнение соседних элементов
+        for index in range(0, count - pass_num - 1):
+            
+            # Если элементы в неправильном порядке
+            if sequence[index] > sequence[index + 1]:
+                # Меняем их местами
+                sequence[index], sequence[index + 1] = sequence[index + 1], sequence[index]
 
-arr = [64, 34, 25, 12, 22, 11, 90]
-bubble_sort(arr)
-print(arr)
+
+def improved_bubble_sort(sequence):
+    """
+    Улучшенная версия с проверкой упорядоченности
+    """
+    count = len(sequence)
+    
+    for pass_num in range(count - 1):
+        # Флаг для отслеживания перестановок
+        has_swapped = False
+        
+        for index in range(0, count - pass_num - 1):
+            if sequence[index] > sequence[index + 1]:
+                sequence[index], sequence[index + 1] = sequence[index + 1], sequence[index]
+                has_swapped = True
+        
+        # Если перестановок не было - массив отсортирован
+        if not has_swapped:
+            break
+
+
+# Демонстрация работы
+if __name__ == "__main__":
+    # Исходные данные
+    original_data = [64, 34, 25, 12, 22, 11, 90]
+    
+    print("Начальный массив:", original_data)
+    
+    # Тестируем обычную версию
+    data1 = original_data.copy()
+    bubble_sort(data1)
+    print("После обычной сортировки:", data1)
+    
+    # Тестируем улучшенную версию
+    data2 = original_data.copy()
+    improved_bubble_sort(data2)
+    print("После улучшенной сортировки:", data2)
